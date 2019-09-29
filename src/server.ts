41b57d9822bea33c1c1951897d39cb208556defd
michael <14226603+mkargus@users.noreply.github.com>
2019-09-29 10:27:01 -0400

    Add nginx files

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import app from './app'
+import fs from 'fs'
 
 // Start Express Server
-app.listen(app.get('port'))
+app.listen(app.get('port'), function(): void {
+  if (process.env.DYNO) {
+    fs.openSync('/tmp/app-initialized', 'w');
+  }
+})
