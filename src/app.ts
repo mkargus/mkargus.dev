fa7d6f057fe177abb7e75ee90e8a8477c89de446
michael <14226603+mkargus@users.noreply.github.com>
2019-09-29 10:36:11 -0400

    Removed helmet & compression

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
-// import compression from 'compression'
 import express from 'express'
-// import helmet from 'helmet'
 import path from 'path'
 import * as route from './route'
 
@@ -11,22 +9,7 @@ const app = express()
 app.set('port', 3000)
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'pug')
-// app.use(compression({ threshold: 0 }))
-// app.use(helmet())
 app.use(express.static('static'))
-// app.use(helmet.contentSecurityPolicy({
-//   directives: {
-//     baseUri: ["'none'"],
-//     defaultSrc: ["'none'"],
-//     fontSrc: ['https://fonts.gstatic.com'],
-//     formAction: ["'none'"],
-//     frameAncestors: ["'none'"],
-//     imgSrc: ["'self'"],
-//     manifestSrc: ["'self'"],
-//     scriptSrc: ["'self'"],
-//     styleSrc: ["'self'", 'https://fonts.googleapis.com']
-//   }
-// }))
 
 // Routes
 app.get('/', route.getIndex)
